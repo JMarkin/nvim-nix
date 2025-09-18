f9a1d2bcf73c63c257b834ce7584d47f72c56c04
jmarkin <me@jmarkin.ru>
2025-09-18 20:41:12 +0300

    ty -> zuban

--- nix/lang/python.nix
+++ nix/lang/python.nix
@@ -1,58 +1,58 @@
 { pkgs, lib, ... }:
-# let
-#
-#   zuban = pkgs.rustPlatform.buildRustPackage
-#     (finalAttrs:
-#       let
-#         typeshedSrc = pkgs.fetchFromGitHub {
-#           owner = "python";
-#           repo = "typeshed";
-#           rev = "1b267b25f2c726291e4e6627b7567f2c8dc04b60";
-#           sha256 = "sha256-SQJypewNYLfNSIc+Myd9l+nypdMdOx+Fyymmg/YpFW4=";
-#         };
-#       in
-#       {
-#         pname = "zuban";
-#
-#         version = "0.0.21";
-#
-#         src = pkgs.fetchFromGitHub {
-#           owner = "zubanls";
-#           repo = "zuban";
-#           tag = "v${finalAttrs.version}";
-#           hash = "sha256-llAJwqJmOtauA7pi7dYZhVH0yFBNN65q2y8ecc9hAJY=";
-#         };
-#
-#         buildAndTestSubdir = "crates/zuban";
-#
-#         postInstall = ''
-#           mkdir -p $out/${pkgs.python3.sitePackages}/zuban
-#           cp -r ${typeshedSrc} $out/${pkgs.python3.sitePackages}/zuban/typeshed
-#         '';
-#
-#         cargoHash = "sha256-sZFc+kjerR7a6JzVb0zQXCfYuJVNnoYUihgLd3qrDLM=";
-#
-#         doInstallCheck = true;
-#
-#         meta = {
-#           description = "Mypy-compatible Python Language Server built in Rust";
-#           homepage = "https://zubanls.com";
-#           # There's no changelog file yet, but they post updates on their blog.
-#           changelog = "https://zubanls.com/blog/";
-#           license = lib.licenses.agpl3Only;
-#           platforms = lib.platforms.all;
-#           mainProgram = "zuban";
-#         };
-#       });
-# in
+let
+
+  zuban = pkgs.rustPlatform.buildRustPackage
+    (finalAttrs:
+      let
+        typeshedSrc = pkgs.fetchFromGitHub {
+          owner = "python";
+          repo = "typeshed";
+          rev = "1b267b25f2c726291e4e6627b7567f2c8dc04b60";
+          sha256 = "sha256-SQJypewNYLfNSIc+Myd9l+nypdMdOx+Fyymmg/YpFW4=";
+        };
+      in
+      {
+        pname = "zuban";
+
+        version = "2025-09-16";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "zubanls";
+          repo = "zuban";
+          rev="9ea8512d8852bd220b6ca8f0763aa50ba7c3c763";
+          sha256="sha256-0yal71pidWCJ01xaz5X9LeE2hNYL75cQ7bzGcuIV/9k=";
+        };
+
+        buildAndTestSubdir = "crates/zuban";
+
+        postInstall = ''
+          mkdir -p $out/${pkgs.python3.sitePackages}/zuban
+          cp -r ${typeshedSrc} $out/${pkgs.python3.sitePackages}/zuban/typeshed
+        '';
+
+        cargoHash = "sha256-TAFdS4NmXchmhqVRcsckz6GhZG35IE2fukDlZiRF8Ms=";
+
+        doInstallCheck = true;
+
+        meta = {
+          description = "Mypy-compatible Python Language Server built in Rust";
+          homepage = "https://zubanls.com";
+          # There's no changelog file yet, but they post updates on their blog.
+          changelog = "https://zubanls.com/blog/";
+          license = lib.licenses.agpl3Only;
+          platforms = lib.platforms.all;
+          mainProgram = "zuban";
+        };
+      });
+in
 {
   packages = with pkgs;
     [
       python313Packages.ipython
 
       ruff
-      ty
-      # zuban
+      # ty
+      zuban
     ];
 
   plugins = [ ];
